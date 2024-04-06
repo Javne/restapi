2ca868bcbfa71227880cd0e3f0f749a051388aed
ewelinaborkowska <javaczysen@gmail.com>
2024-04-06 11:49:16 +0200

    Initial commit

---
+++ src/main/java/com/javne/restapi/Student.java
@@ -0,0 +1,15 @@
+//package com.javne.restapi;
+//
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.GeneratedValue;
+//import jakarta.persistence.Id;
+//
+//@Entity
+//public class Student {
+//    @Id
+//    @GeneratedValue
+//    private Long id;
+//    private String name;
+//    private Long passportNumber;
+//}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO post (title, content) VALUES ('Pierwszy post', 'Treść pierwszego posta...');
+INSERT INTO post (title, content) VALUES ('Drugi post', 'Treść drugiego posta...');
