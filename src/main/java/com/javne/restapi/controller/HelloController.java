c44b223de4580d90a15c8a723357375c61d84f79
ewelinaborkowska <javaczysen@gmail.com>
2024-04-06 13:15:59 +0200

    post and comments generated by gradle groovy taskMethod

--- src/main/java/com/javne/restapi/controller/HelloController.java
+++ src/main/java/com/javne/restapi/controller/HelloController.java
@@ -2,18 +2,18 @@ package com.javne.restapi.controller;
 
 
 import com.javne.restapi.service.HelloService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HelloController {
 
-    @Autowired
-    private HelloService helloService;
+    private final HelloService helloService; // wstrzykniecie za pomoca konstruktora stworzonego przez lomboka
 
     @GetMapping("/")
-    public String hello(){
+    public String hello() {
         return helloService.hello();
     }
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,5 +1,22 @@
+-- Usunięcie tabel, jeśli istnieją
+DROP TABLE IF EXISTS comment;
+DROP TABLE IF EXISTS post;
+
+-- Tworzenie tabel post i comment
 CREATE TABLE post (
    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-   title VARCHAR(200) NOT NULL,
-   content VARCHAR(2000)
+   title VARCHAR(400) NOT NULL,
+   content VARCHAR(2000),
+   created TIMESTAMP
 );
+
+CREATE TABLE comment (
+   id BIGINT PRIMARY KEY AUTO_INCREMENT,
+   post_id BIGINT NOT NULL,
+   content VARCHAR(2000) NULL,
+   created TIMESTAMP,
+   FOREIGN KEY (post_id) REFERENCES post(id)
+);
+
+
+
